223e9fcf7588fdb69ba0d8750f660133432e0d4e
esc20936 <esc20936@uvg.edu.gt>
2022-09-12 13:41:19 -0600

    first commit

---
+++ BMPImageHandler.java
@@ -0,0 +1,12 @@
+public class BMPImageHandler {
+    
+    static void print(String s) {
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        print(args[0]);
+        print(args[1]);
+    }
+
+
+}
